f59131cde1431ca3d6590ef9971b2daba381484d
coelhojs <ccoelho.me@gmail.com>
2018-10-21 13:41:13 +0000

    ok

---
+++ Trabalho 01/src/Carro.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+
+public class Carro {
+	private int velocidade;
+	private double preco, tanque, portaMala;
+	private String cor, pneusMarca;
+	private LocalDate ultimaRevisao, dataCompra, dataPneus;
+
+	public void abastecer(double litros) {
+
+	}
+	
+	public void revisao() {
+		
+	}
+	
+	public double vender() {
+		
+	}
+	
+	public boolean aptoViajar() {
+		
+	}
+	
+	public tanqueCheio() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
