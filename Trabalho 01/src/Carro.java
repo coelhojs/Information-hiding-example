93c2ff7ef88afbb87f06581c72560383a6cc6e18
coelhojs <ccoelho.me@gmail.com>
2018-10-21 14:56:53 +0000

    ok

--- Trabalho 01/src/Carro.java
+++ Trabalho 01/src/Carro.java
@@ -1,34 +1,64 @@
+
+//AvoidPublicFields
+//DontReturnMutableCollectionsOrArrays
+//AvoidUncheckedParametersOfSetters
+//UseInterfacesAsReturnType
+//AvoidProtectedFields
+//AvoidSettersForHeavilyUsedFields
+//AvoidManySetters
+//AvoidManyGetters
+//UseImmutableObjects
+//DontExposeInternalStructureOfClass
+//AvoidExposingImplementationDetailsInMethodN
+//MakeClassesPublicIfNecessary
+//CheckInterrelatednessOfLayer
+//AvoidReturningCollectionsArrays
+
 import java.time.LocalDate;
 
 public class Carro {
-	private int velocidade;
-	private double preco, tanque, portaMala;
-	private String cor, pneusMarca;
-	private LocalDate ultimaRevisao, dataCompra, dataPneus;
+	private int anoFabricacao, anoModelo, portaMala;
+	private double precoCompra, precoVenda, tanque;
+	private boolean jaBateu;
+	private String cor, pneusMarca, estadoProcedencia;
+	private LocalDate ultimaRevisao, dataCompra, dataPneus, hoje;
 
 	public void abastecer(double litros) {
 
 	}
-	
+
+	public double setPrecoVenda(double precoCompra) {
+		if (jaBateu) {
+
+		} else if (anoFabricacao <= 3) {
+			// - Divida o valor do carro zero km (exemplo: R$35.000) por 5, ​para saber o
+			// valor anual de depreciação (R$35.000 / 5 = R$7.000);
+			// - Divida esse valor (R$7.000) por 12, que é o número de meses do ano; ​
+			// ​- O valor obtido (R$583,33) representa o quanto o carro vai desvalorizar a
+			// cada mês. Ou seja, depois de sair da concessionária, você pode considerar
+			// que, a cada mês, o carro em questão depreciará aproximadamente R$580. A conta
+			// é fácil, não é? ​
+		} else {
+
+		}
+		return this.precoVenda;
+	}
+
 	public void revisao() {
-		
+
 	}
-	
-	public double vender() {
-		
+
+	public boolean aptoViajar(LocalDate ultimaRevisao) {
+		int periodo = ultimaRevisao.getDayOfYear() - hoje.getDayOfYear();
+		if (periodo >= 1) {
+			return false;
+
+		} else {
+			return true;
+		}
 	}
-	
-	public boolean aptoViajar() {
-		
-	}
-	
+
 	public tanqueCheio() {
 		
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
---
+++ Trabalho 01/src/Frota.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Frota {
+
+	
+
+	public double vender() {
+
+	}
+
+	public static void main(String[] args) {
+		List<Carro> frota = new ArrayList<Carro>();
+		
+
+	}
+
+}
